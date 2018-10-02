4ae92d8aa61fc8d6bdfc218f1f73861e9a5230ef
Linus Chan <chanx228@umn.edu>
2018-10-02 13:44:40 -0700

    test
    test

--- sendmail.php
+++ sendmail.php
@@ -4,5 +4,6 @@
 
   mail( "layjchan@gmail.com", "Feedback Form Results",
     $message, "From: $email" );
+  echo "Thank you for using our mail form";
   header( "Location: http://www.example.com/thankyou.html" );
 ?>
